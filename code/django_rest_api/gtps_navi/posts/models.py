c13a6b4785ae463f90f8339853dea9a5c8bfd0ac
dueityou <dueityou@gmail.com>
2023-11-13 01:12:07 +0900

    各モデルの設計完了

--- code/django_rest_api/gtps_navi/posts/models.py
+++ code/django_rest_api/gtps_navi/posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
+from tags.models import Tag
 
-# Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='posts_images/')
+    url = models.URLField()
+    tags = models.ManyToManyField(Tag, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
--- code/django_rest_api/gtps_navi/users/models.py
+++ code/django_rest_api/gtps_navi/users/models.py
@@ -1,20 +1,15 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
-class CustomUser(AbstractUser):
-    # OAuthで取得したユーザー名
-    username = models.CharField(max_length=150, unique=True)
+class User(models.Model):
+    # ここでユーザーとの一対多の関連を定義
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='posts_images/')
+    url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    # 固有のランダムなID（例：UUID）
-    unique_id = models.CharField(max_length=100, unique=True)
-
-    # プロフィール画像（オプション）
-    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
-
-    # プロフィール自己紹介（オプション）
-    bio = models.TextField(blank=True, null=True)
-
-    # OAuth認証情報（TwitterのID）
-    twitter_id = models.CharField(max_length=100, blank=True, null=True)
-
-    # 追加のフィールドとメソッド（必要に応じて）
+    def __str__(self):
+        return self.name
